3e332b1fee5b133e3e3704749b4ae7fa088b1113
julia <juliakofmana@gmail.com>
2023-11-15 21:57:49 +0300

    Make main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+	employee_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes varchar(200) NOT NULL
+);
+
+
+CREATE TABLE customers(
+	customer_id text UNIQUE,
+	company_name text,
+	contact_name text
+);
+
+CREATE TABLE orders(
+	order_id int PRIMARY KEY,
+	customer_id text REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city text
+);
+
+SELECT * FROM employees;
+SELECT * FROM customers;
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,45 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+# Подключение к базе данных
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="J3h7k2")
+
+# Создание курсора
+try:
+    with conn:
+        cur = conn.cursor()
+        with open("north_data/employees_data.csv", "r", encoding="utf-8") as file:
+            # Загрузка данных из файла "employees.csv" в таблицу employees
+            reader = csv.DictReader(file)
+            for row in reader:
+                cur.execute(
+                    f'INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes)'
+                    f'VALUES (%s, %s, %s, %s, %s, %s)',
+                    (row["employee_id"], row["first_name"], row["last_name"], row["title"], row["birth_date"],
+                     row["notes"]))
+
+        with open("north_data/customers_data.csv", "r", encoding="utf-8") as file:
+            # Загрузка данных из файла "customers.csv" в таблицу customers
+            reader = csv.DictReader(file)
+            for row in reader:
+                cur.execute(
+                    f'INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)',
+                    (row["customer_id"], row["company_name"], row["contact_name"]))
+
+        with open("north_data/orders_data.csv", "r", encoding="utf-8") as file:
+            # Загрузка данных из файла "orders.csv" в таблицу orders
+            reader = csv.DictReader(file)
+            for row in reader:
+                cur.execute(
+                    f'INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city)'
+                    f'VALUES (%s, %s, %s, %s, %s)',
+                    (row["order_id"], row["customer_id"], row["employee_id"], row['order_date'], row['ship_city']))
+
+        # Сохранение изменений
+        # conn.commit()
+
+finally:
+    # Закрытие курсора и соединения
+    cur.close()
+    conn.close()
